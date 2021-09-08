c519347c49a57c5667792f0b163f6e97d3e1d171
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2021-09-08 16:08:34 +0900

    fix

--- get-region-of-instance.sh
+++ get-region-of-instance.sh
@@ -1,5 +1,6 @@
 script=https://raw.githubusercontent.com/iizus/ec2-launch-template/main/get-token-of-metadata.sh
-curl -s $script | bash
+TOKEN=`curl -s $script | bash`
 
+IP=http://169.254.169.254/latest
 region=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v $IP/meta-data/placement/availability-zone | sed -e 's/.$//g'`
 echo $region
