ba2b813a31be24a10a9da14c6155fa72944bf41d
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2021-09-08 21:20:55 +0900

    modify

--- main.sh
+++ main.sh
@@ -4,6 +4,6 @@ yum upgrade -y
 yum install -y tree jq
 
 script=https://raw.githubusercontent.com/iizus/ec2-launch-template/main/set-date-to-instance-name.sh
-name=$(curl -s $script | bash)
+name=$(curl -s ${script} | bash)
 
 echo ${name}
