4373009a08af215b80d745200f717f1412e8a7e3
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2021-09-08 16:09:15 +0900

    fix

--- get-instance-id.sh
+++ get-instance-id.sh
@@ -1,5 +1,7 @@
 script=https://raw.githubusercontent.com/iizus/ec2-launch-template/main/get-token-of-metadata.sh
-curl -s $script | bash
+TOKEN=`curl -s $script | bash`
 
+IP=http://169.254.169.254/latest
 id=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v $IP/meta-data/instance-id`
+
 echo $id
