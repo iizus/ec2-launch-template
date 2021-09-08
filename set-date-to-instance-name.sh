b253233f363e49eb0f722c586a169751034af532
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2021-09-08 16:40:55 +0900

    add echo

--- set-date-to-instance-name.sh
+++ set-date-to-instance-name.sh
@@ -9,3 +9,5 @@ script=https://raw.githubusercontent.com/iizus/ec2-launch-template/main/get-regi
 region=`curl -s $script | bash`
 
 aws ec2 create-tags --resources $id --tags Key=Name,Value=$name --region $region
+
+echo $name
