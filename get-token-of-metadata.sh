2653992b2d9a3003feeab177d78f89d5450b6dd9
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2021-09-08 16:00:16 +0900

    add export command

--- get-token-of-metadata.sh
+++ get-token-of-metadata.sh
@@ -1,3 +1,3 @@
-IP=http://169.254.169.254/latest
-TOKEN=`curl -X PUT "$IP/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+export IP=http://169.254.169.254/latest
+export TOKEN=`curl -X PUT "$IP/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 echo $TOKEN
