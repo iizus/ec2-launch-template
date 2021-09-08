6110f97cbcb6473eb6a28490b6d08d70136439c1
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2021-09-08 21:23:31 +0900

    modify

--- install-ggv2.sh
+++ install-ggv2.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 script=https://raw.githubusercontent.com/iizus/ec2-launch-template/main/main.sh
-name=`curl -s $script | bash`
+name=$(curl -s ${script} | bash)
 
 yum install -y java-11-amazon-corretto-headless
 
 nucleus=greengrass-nucleus-latest.zip
 dir=GreengrassCore
-url=https://d2s8p88vqu9w66.cloudfront.net/releases/$nucleus
+url=https://d2s8p88vqu9w66.cloudfront.net/releases/${nucleus}
 
 cd /home/ec2-user
-wget $url
-unzip $nucleus -d $dir
+wget ${url}
+unzip ${nucleus} -d ${dir}
 
-echo $name
-java -Droot="/greengrass/v2" -Dlog.store=FILE -jar ./$dir/lib/Greengrass.jar --thing-name $name --component-default-user ggc_user:ggc_group --provision true --setup-system-service true --deploy-dev-tools true
+echo ${name}
+
+JAR=$(./${dir}/lib/Greengrass.jar)
+java -Droot="/greengrass/v2" -Dlog.store=FILE -jar ${JAR} --thing-name $name --component-default-user ggc_user:ggc_group --provision true --setup-system-service true --deploy-dev-tools true
