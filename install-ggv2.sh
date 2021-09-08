bb9d4e9fb95fdd1bbe2fe2ec6df218bd0bb45b2e
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2021-09-08 20:33:53 +0900

    modify

--- install-ggv2.sh
+++ install-ggv2.sh
@@ -6,8 +6,10 @@ name=`curl -s $script | bash`
 yum install -y java-11-amazon-corretto-headless
 
 nucleus=greengrass-nucleus-latest.zip
+dir=GreengrassCore
 url=https://d2s8p88vqu9w66.cloudfront.net/releases/$nucleus
-wget $url
-unzip greengrass-nucleus-latest.zip -d GreengrassCore
 
-java -Droot="/greengrass/v2" -Dlog.store=FILE -jar ./GreengrassCore/lib/Greengrass.jar --thing-name $name --component-default-user ggc_user:ggc_group --provision true --setup-system-service true --deploy-dev-tools true
+wget $url
+unzip $nucleus -d $dir
+
+java -Droot="/greengrass/v2" -Dlog.store=FILE -jar ./$dir/lib/Greengrass.jar --thing-name $name --component-default-user ggc_user:ggc_group --provision true --setup-system-service true --deploy-dev-tools true
