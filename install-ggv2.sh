a0cb818147d80d4bb3dcd40698d3c1f823d672d1
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2021-09-08 16:53:37 +0900

    add gg install

--- install-ggv2.sh
+++ install-ggv2.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
 script=https://raw.githubusercontent.com/iizus/ec2-launch-template/main/main.sh
-curl -s $script | bash
+name=`curl -s $script | bash`
+
+yum install -y java-11-amazon-corretto-headless
+
+nucleus=greengrass-nucleus-latest.zip
+url=https://d2s8p88vqu9w66.cloudfront.net/releases/$nucleus
+wget $url
+unzip greengrass-nucleus-latest.zip -d GreengrassCore
+
+sudo -E java -Droot="/greengrass/v2" -Dlog.store=FILE -jar ./GreengrassCore/lib/Greengrass.jar \
+  --thing-name $name \
+  --component-default-user ggc_user:ggc_group \
+  --provision true \
+  --setup-system-service true \
+  --deploy-dev-tools true
