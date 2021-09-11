c62c2f5e1972f57e05536c9b07b2a0c051a6f9b6
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2021-09-11 12:01:27 +0900

    remove yum install

--- install-ggv2.sh
+++ install-ggv2.sh
@@ -3,9 +3,6 @@
 script=https://raw.githubusercontent.com/iizus/ec2-launch-template/main/set-date-to-instance-name.sh
 name=$(curl -s ${script} | bash)
 
-yum upgrade -y
-yum install -y tree jq java-11-amazon-corretto-headless
-
 nucleus=greengrass-nucleus-latest.zip
 dir=GreengrassCore
 url=https://d2s8p88vqu9w66.cloudfront.net/releases/${nucleus}
