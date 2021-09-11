b64f92b771f1f3df94809c470d7af47d50dfb720
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2021-09-11 12:00:22 +0900

    remove yum install

--- main.sh
+++ main.sh
@@ -2,8 +2,4 @@
 
 script=https://raw.githubusercontent.com/iizus/ec2-launch-template/main/set-date-to-instance-name.sh
 name=$(curl -s ${script} | bash)
-
 echo ${name}
-
-yum upgrade -y
-yum install -y tree jq
