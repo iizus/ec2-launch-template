45ff806322440a268f7f20ff4fe9b194efb77d71
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2021-12-23 17:20:43 +0900

    update

--- install-ggv2.sh
+++ install-ggv2.sh
@@ -20,11 +20,11 @@ java \
   -Droot="/greengrass/v2" \
   -Dlog.store=FILE -jar ${JAR} \
   --thing-name ${name} \
-  --thing-policy-name Full-Access-Policy \
   --thing-group-name Greengrass_v2 \
   --component-default-user ggc_user:ggc_group \
   --provision true \
+  --start true \
   --setup-system-service true \
   --deploy-dev-tools true \
   --tes-role-name GreengrassV2TokenExchangeRole \
-  --tes-role-alias-name GreengrassCoreTokenExchangeRoleAlias
+  --tes-role-alias-name GreengrassV2TokenExchangeRoleAlias
