d155d347e2468a9eef91586fdcbadcaf6aa1f538
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2023-02-19 21:27:11 +0900

    update

--- install-ggv2.sh
+++ install-ggv2.sh
@@ -3,17 +3,8 @@
 script=https://raw.githubusercontent.com/iizus/ec2-launch-template/main/set-date-to-instance-name.sh
 name=$(curl -s ${script} | bash)
 
-yum upgrade -y
-yum install -y tree jq java-11-amazon-corretto-headless
-
-nucleus=greengrass-nucleus-latest.zip
-dir=GreengrassCore
-url=https://d2s8p88vqu9w66.cloudfront.net/releases/${nucleus}
-
 cd /home/ec2-user
-wget ${url}
-unzip ${nucleus} -d ${dir}
-
+dir=GreengrassCore
 JAR=./${dir}/lib/Greengrass.jar
 
 java -Droot="/greengrass/v2" -Dlog.store=FILE -jar ${JAR} \
